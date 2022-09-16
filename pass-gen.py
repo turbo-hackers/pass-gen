ee481bea5e52f77b3fb324962ba1e03943dd6a1c
TURB0 <turbozhacker@gmail.com>
2022-09-17 03:46:43 +0500

    initial commit

--- pass-gen.py
+++ pass-gen.py
@@ -1,23 +1,28 @@
-#!/bin/python
+#!/usr/bin/env python3
+# coded by TURB0
+# github https://github.com/turbo-hackers
+
 import random,os,sys,time
 
 #colors
 yellow ='\033[93m'
 green ='\033[92m'
 cyan ='\033[96m'
-black ='\033[1m'
+reset = '\033[0m'
+blink = "\033[5;96m"
 #Banner Code
 def banner():
 	r ='\033[91m' # red
 	g ='\033[92m' # green
 	b ='\033[96m' # blue
 	y ='\033[93m' # yellow
-	bl ='\033[1m' # black
+	reset = '\033[0m'
+	blink = "\033[5;96m"
 	os.system("clear")
-	print(g+bl+"Tool is Starting....../"+g+bl)
+	print(g+"Tool is Starting....../"+reset)
 	time.sleep(1)
 	os.system("clear")
-	print (b+bl+"""
+	print (b+"""
 	        _____
 	       / ___ \\
 	      / /   \\ \\
@@ -28,26 +33,26 @@ def banner():
 	║ \|/ \|/ \|/  ___  ║
 	║ /|\ /|\ /|\ |___| ║
 	╚═══════════════════╝ v 2.0 
-	"""+bl+b)
+	"""+reset)
 	print (" ")
-	print (y+bl+"                      <===[["+b+bl+" coded by TURB0 "+y+bl+"]]===>"+y+bl+"\n")
-	print (y+bl+"                   <---("+r+bl+" GitHub- Turbo Hackers "+y+bl+")--->"+y+bl+"\n")
+	print (y+"                      <===[["+b+" coded by"+blink+" TURB0 "+reset+y+"]]===>"+y+"\n")
+	print (y+"                   <---("+r+" GitHub- Turbo Hackers "+y+")--->"+y+"\n"+reset)
 
 #gen code
 def gen():
-	length = int(input(cyan+black+"\n\nEnter The Length Of The Password: "+cyan+black))
+	length = int(input(cyan+"\n\nEnter The Length Of The Password: "+reset))
 	char = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890@#$%^&!*_-+=?/\\:;"
 	print (" ")
-	print (yellow+black+"-------> Password Generated <-------"+yellow+black)
+	print (yellow+"-------> Password Generated <-------"+reset)
 	print (" ")
-	password = (green+black+" "+green+black)
+	password = (green+" "+reset)
 	for i in range(length):
 		
 		password += random.choice(char)
 		
 	print ("\t"+password)
 	print (" ")
-	print (yellow+black+"-------> Take Your Password <-------"+yellow+black)
+	print (yellow+"-------> Take Your Password <-------"+reset)
 def main():
 	banner()
 	gen()
